61e2873f5111df04dd5162753bd97bb290c27b67
HyperSimon <hero21c@outlook.com>
2017-09-11 14:16:04 +0800

    移除 无用prop style；修改 默认属性为static

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 /**
- * Created by simon on 2017/3/15.
+ * Created by simon on 2017/3/22.
  */
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  View,
-} from 'react-native';
+import React, {Component, PropTypes} from 'react';
+import {View} from 'react-native';
 
 export default class VisibleView extends Component {
 
   static propTypes = {
-    visible: React.PropTypes.bool
+    visible: PropTypes.bool,
+  }
+
+  static getDefaultProps() {
+    return {
+      visible: true,
+    }
   }
 
   render() {
-    const comp = this.props.visible
-      ? <View>{this.props.children}</View>
-      : null
-
-    return comp
+    return this.props.visible ? this.props.children : null
   }
 }
